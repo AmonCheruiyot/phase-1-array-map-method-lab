0f82c548de02d30da8bf411538cfcaee5047a4bf
AmonCheruiyot <161847144+AmonCheruiyot@users.noreply.github.com>
2024-03-25 16:12:51 +0300

    complets test

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased() {
+  const titleCasedNames = tutorials.map(function(name)
+  { return name.split(' ').map(function(word)
+  { return word.charAt(0).toUpperCase() + word.slice(1); }).join(' '); });
+   return titleCasedNames;
 }
